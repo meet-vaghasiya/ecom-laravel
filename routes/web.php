c2fe64263a6b4bf516cbfe5850e1d101d61320d1
meet-vaghasiya <meetvaghasiya2012@gmail.com>
2021-08-01 12:30:29 +0530

    installing  jetstream and livewire

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+
+Route::prefix('admin')->group(function(){
+    Route::prefix('users')->group(function(){
+        Route::get('/',[UserController::class,'index']);
+    });
 });
